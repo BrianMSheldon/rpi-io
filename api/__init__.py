0b492dce5fd71639948528ae59dcade9904ab556
Brian M. Sheldon <brianmsheldon@gmail.com>
2019-12-30 21:05:57 -0500

    Fix NotImplementedError on Windows

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,8 @@ import logging
 from tornado.ioloop import IOLoop
 from tornado.web import Application
 
+from api import compat
+
 # Configure logging
 log = logging.getLogger()
 
@@ -15,10 +17,14 @@ def make_app():
 
 def start(port: int = 8100):
     app = make_app()
-    app.listen(port)
+    try:
+        app.listen(port)
+    except NotImplementedError:
+        raise NotImplementedError('asyncio event loop policy may need to be set')
     IOLoop.current().start()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
+    compat.event_loop_policy()
     start()
---
+++ api/compat.py
@@ -0,0 +1,34 @@
+"""
+This module handles various compatibility issues.
+"""
+
+import asyncio
+import logging
+import sys
+
+import tornado
+
+# Configure logging
+log = logging.getLogger()
+
+
+def event_loop_policy():
+    """Set the event loop policy."""
+    windows_event_loop_policy()
+
+
+def windows_event_loop_policy():
+    """Set the asyncio event loop policy for Windows."""
+
+    # On Windows with Python 3.8 and Tornado 5+ a "selector" event loop is required
+    # Reference:
+    #     https://github.com/tornadoweb/tornado/issues/2608
+
+    if all([
+        sys.platform == 'win32',
+        sys.version_info > (3, 8),
+        tornado.version_info > (5,),
+    ]):
+        policy = asyncio.WindowsSelectorEventLoopPolicy()
+        log.debug('Configuring event loop policy')
+        asyncio.set_event_loop_policy(policy)
